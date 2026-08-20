47ed83a3151ebd36bf72fcb80160aed02311e6d4
신현주 <sinhju49@gmail.com>
2026-08-20 11:38:56 +0900

    레이아웃 완성: Canvas Overlay + 비율 기반 좌우 분할, 배경 카메라 고정

--- Assets/Scripts/MoonClimbFaceRunner.cs
+++ Assets/Scripts/MoonClimbFaceRunner.cs
@@ -66,6 +66,11 @@ namespace Mediapipe.Unity.Sample.FaceLandmarkDetection
 
       // NOTE: The screen will be resized later, keeping the aspect ratio.
       screen.Initialize(imageSource);
+      var rt = screen.GetComponent<RectTransform>();
+      rt.anchorMin = Vector2.zero;
+      rt.anchorMax = Vector2.one;
+      rt.offsetMin = Vector2.zero;
+      rt.offsetMax = Vector2.zero;
 
       SetupAnnotationController(_faceLandmarkerResultAnnotationController, imageSource);
 
